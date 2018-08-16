17f7d23ec3f53748969aa51058913b24681fd54a
Sundar <aw57rain@gmail.com>
2018-08-16 20:55:46 +0530

    First commit

---
+++ items.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class TimecoverspiderItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+class MagazineCover(scrapy.Item):
+    title = scrapy.Field()
+    PubDate = scrapy.Field()
+    file_urls = scrapy.Field()
+    files = scrapy.Field()
+
+class ExponeaImages(scrapy.Item):
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
